899d27ad68b678b30ee83f0000faa000625d36d8
Kanst <ae_jeka@mail.ru>
2012-10-05 20:51:10 +0400

    Настроен zsh

--- cookbooks/oh-my-zsh/recipes/default.rb
+++ cookbooks/oh-my-zsh/recipes/default.rb
@@ -1,15 +1,15 @@
 #
-# Cookbook Name:: htop
+# Cookbook Name:: oh-my-zsh
 # Recipe:: default
 #
-# Copyright 2010, Heavy Water Software Inc.
+# Copyright 2011, Heavy Water Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,5 +17,19 @@
 # limitations under the License.
 #
 
-package "htop"
 
+
+node['oh_my_zsh']['users'].each do |u|
+
+  git "/home/#{u}/.oh-my-zsh" do
+  repository "https://github.com/Kanst/oh-my-zsh"
+  reference "master"
+  action :sync
+  end 
+
+  template "/home/#{u}/.zshrc" do
+    source "zshrc.erb"
+    mode 0774
+    action :create
+  end
+end
--- cookbooks/zsh/recipes/default.rb
+++ cookbooks/zsh/recipes/default.rb
@@ -1,15 +1,15 @@
 #
-# Cookbook Name:: htop
+# Cookbook Name:: zsh
 # Recipe:: default
 #
-# Copyright 2010, Heavy Water Software Inc.
+# Copyright 2008-2009, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,5 +17,17 @@
 # limitations under the License.
 #
 
-package "htop"
+package "zsh" do
+  action :install
+end
 
+case node['platform_family']
+when "debian"
+  package "zsh-doc" do
+    action :install
+  end
+when "rhel", "fedora"
+  package "zsh-html" do
+    action :install
+  end
+end
